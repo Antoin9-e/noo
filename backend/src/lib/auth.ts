6458ed034434ca62980a878f7751b4faa231e9d9
Antoin9-e <antoine.matter@etu.unicaen.fr>
2026-01-22 22:18:23 +0100

    add google auth

--- backend/src/lib/auth.ts
+++ backend/src/lib/auth.ts
@@ -19,4 +19,11 @@ export const auth = betterAuth({
   },
   trustedOrigins: ["http://localhost:5173"],
   baseURL: process.env.BETTER_AUTH_URL || "http://localhost:3000",
+  socialProviders: {
+    google: {
+      prompt: "select_account",
+      clientId: process.env.GOOGLE_CLIENT_ID || "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
+    },
+  },
 });
--- frontend/src/components/Auth/SocialProviders.tsx
+++ frontend/src/components/Auth/SocialProviders.tsx
@@ -1,6 +1,13 @@
 import { FaGoogle, FaApple } from "react-icons/fa";
+import { authClient } from "@/lib/auth-client";
 
 export default function SocialProviders() {
+  const HandleGoogleSignIn = async () => {
+    const data = await authClient.signIn.social({
+      provider: "google",
+      callbackURL: "http://localhost:5173/",
+    });
+  };
   return (
     <div className="w-full flex flex-col">
       <div className="w-full border-2 border-gray-200"></div>
@@ -10,7 +17,12 @@ export default function SocialProviders() {
         </p>
         <div className="flex gap-x-2 items-center justify-center">
           <div className="flex items-center justify-center flex-col">
-            <FaGoogle className="w-10 h-10 p-2 border-2 border-gray-200 rounded-lg cursor-pointer text-orange-500 hover:bg-gray-100" />
+            <FaGoogle
+              onClick={() => {
+                HandleGoogleSignIn();
+              }}
+              className="w-10 h-10 p-2 border-2 border-gray-200 rounded-lg cursor-pointer text-orange-500 hover:bg-gray-100"
+            />
             <p className="text-sm text-gray-500">Google</p>
           </div>
           <div className="flex items-center justify-center flex-col">
